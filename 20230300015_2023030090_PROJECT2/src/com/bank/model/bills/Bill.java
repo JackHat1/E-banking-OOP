b7c3136729ecc8e095043d41bfca388e7f322894
Mitsqrqs <dimkonto70@gmail.com>
2025-05-05 19:19:22 +0300

    bill

--- 20230300015_2023030090_PROJECT2/src/com/bank/model/bills/Bill.java
+++ 20230300015_2023030090_PROJECT2/src/com/bank/model/bills/Bill.java
@@ -1,7 +1,81 @@
 package com.bank.model.bills;
 
+import java.time.LocalDate;
 
 public class Bill {
+    private String paymentCode;
+    private String billNumber;
+    private double amount;
+    private String issuer;
+    //private String customer;
+    private LocalDate issueDate;
+    private LocalDate dueDate;
 
+
+    public Bill(String paymentCode, String billNumber, String issuer, double amount, LocalDate issueDate, LocalDate dueDate) {
+        this.paymentCode = paymentCode;
+        this.billNumber = billNumber;
+        this.issuer = issuer;
+       // this.customer = customer;
+        this.amount = amount;
+        this.issueDate = issueDate;
+        this.dueDate = dueDate;
+
+    }
+
+
+    private String getPaymentCode() {
+        return paymentCode;
+    }
+
+    private void setPaymentCode(String paymentCode) {
+        this.paymentCode = paymentCode;
+    }
+
+    private String getBillNumber() {
+        return billNumber;
+    }
+
+    private void setBillNumber(String billNumber) {
+        this.billNumber = billNumber;
+    }
+
+    private double getAmount() {
+        return amount;
+    }
+
+    private void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    private String getIssuer() {
+        return issuer;
+    }
+
+    private void setIssuer(String issuer) {
+        this.issuer = issuer;
+    }
+
+    private LocalDate getIssueDate() {
+        return issueDate;
+    }
+
+    private void setIssueDate(LocalDate issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    private LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    private void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+
+    
+    public String printBill(){
+        return "Bill{ "+ "Payment Code: "+ paymentCode + ", Bill Number: " + billNumber + ", Issuer: " + issuer + ", Amount: " + amount + ", Issue Date: " + issueDate + ", Due Date: " + dueDate + '}';
+    }
     
 }
--- 20230300015_2023030090_PROJECT2/src/com/bank/model/transactions/Payment.java
+++ 20230300015_2023030090_PROJECT2/src/com/bank/model/transactions/Payment.java
@@ -3,14 +3,17 @@ package com.bank.model.transactions;
 import com.bank.model.accounts.Account;
 import com.bank.model.users.User;
 
+import com.bank.model.bills.Bill;
+
 public class Payment extends Transaction {
     private Account from;
     private Account business;
     private double amount;
-    private String paymentCode; // RF
+    //private String paymentCode; // RF
+    private Bill paymentCode;
     private String billReason;
 
-    public Payment(Account from, Account business, double amount, String rfCode, User transactor, String billReason) {
+    public Payment(Account from, Account business, double amount, Bill rfCode, User transactor, String billReason) {
         super(transactor, "Payment");
         this.from = from;
         this.business = business;
