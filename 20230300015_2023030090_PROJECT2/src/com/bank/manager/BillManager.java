0061a4799c29b5f14285419bf978d24261a823e4
Mitsqrqs <dimkonto70@gmail.com>
2025-05-05 20:02:20 +0300

    νεο

--- 20230300015_2023030090_PROJECT2/src/com/bank/manager/BillManager.java
+++ 20230300015_2023030090_PROJECT2/src/com/bank/manager/BillManager.java
@@ -1,5 +1,33 @@
 package com.bank.manager;
 
+import java.util.ArrayList;
+import java.util.List;
+import com.bank.model.bills.Bill;
+
 public class BillManager {
     
+    private final List<Bill> bills = new ArrayList<>();
+    private final String[] path = {
+        "./data/bills/2025-05-01.csv",
+        "./data/bills/2025-05-05.csv",
+        "./data/bills/2025-05-20.csv",
+        "./data/bills/2025-06-01.csv"
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- 20230300015_2023030090_PROJECT2/src/com/bank/manager/StandingOrderManager.java
+++ 20230300015_2023030090_PROJECT2/src/com/bank/manager/StandingOrderManager.java
@@ -1,5 +1,15 @@
 package com.bank.manager;
 
+import java.util.ArrayList;
+import java.util.List;
+import com.bank.model.orders.StandingOrder;
+
 public class StandingOrderManager {
+
+    private final List<StandingOrder> orders= new ArrayList<>();
+    private final String path = ".data/orders/active.csv";
+
+
+
     
 }
