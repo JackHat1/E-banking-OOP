7bcc2bccc715aab2ab9e3aad714621d711f41a00
Mitsqrqs <dimkonto70@gmail.com>
2025-05-09 19:33:40 +0300

    new

--- 20230300015_2023030090_PROJECT2/src/com/bank/model/statements/StatementEntry.java
+++ 20230300015_2023030090_PROJECT2/src/com/bank/model/statements/StatementEntry.java
@@ -1,5 +1,14 @@
 package com.bank.model.statements;
 
+import com.bank.model.transactions.*;
+
 public class StatementEntry {
+
+   // private Transaction transactor;
+    //private Transaction timestamp;
+    //private Transaction reason;
+    //private String type; // debit ,credit
+    private double amount;
+    private d
     
 }
